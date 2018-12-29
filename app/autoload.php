21f6800637eaa2dbb82ae43ee2ab850bd084a974
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-12-29 17:56:59 +0100

    get userInfo from logged in user in autoload-file

--- app/autoload.php
+++ app/autoload.php
@@ -20,7 +20,17 @@ $config = require __DIR__.'/config.php';
 // Setup the database connection.
 $pdo = new PDO($config['database_path']);
 
+$loggedInUser = null;
 
-
+if ($_SESSION['user']) {
+    $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+    $id = $_SESSION['user'];
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $loggedInUser = $statement->fetch(PDO::FETCH_ASSOC);
+}
 
 //Detta är kopierat från uppgift 28, kolla igeon om allt behövs

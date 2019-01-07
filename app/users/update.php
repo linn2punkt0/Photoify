041a26ed4507c40495c729426359ec98109e08ce
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-07 15:52:53 +0100

    maybe almost done with user-update

---
+++ app/users/update.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+// In this file we register users.
+
+$errors = [];
+
+// Check if current password is submitted
+if ($_POST['current-password']) {
+    
+// Check if submitted password matches database
+    if(password_verify($_POST['current-password'], $loggedInUser['password'])){
+
+// Check if image is submitted
+    if (isset($_FILES['image'])) {
+        $image = $_FILES['image'];
+
+    // Check if image is correct file type, if not, store error message
+        if (!in_array($image['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+            $errors[] = 'The uploaded file type is not allowed.';
+        }
+
+    // Check if image is correct size, if not, store error message
+        if ($image['size'] > 2097152) {
+            $errors[] = 'The uploaded file exceeded the filesize limit.';
+        }}
+    // Check if the errors any contains any errors, if not, continue
+        if (count($errors) === 0) {
+
+        // Sanitize all fields
+            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+            $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $id = $loggedInUser['id'];
+
+        // If email exists in other account, or if passwords do not match, add message to errors array.
+            if ($notNewEmail && !$id) {
+                $errors [] = "Email is used by other account!";
+            }
+            if ($_POST['new-password'] !== $_POST['password-control']) {
+                $errors [] = "New passwords did not match, try again!";
+            }
+        // If new passwords match, save the new password
+            else {
+                $newPassword = $_POST['new-password'];
+            }
+
+        // Check if the errors any contains any errors, if not, continue
+            if (count($errors) === 0) {
+
+                $updateUser = $pdo->prepare('UPDATE users (email, "password", profile_image_url, biography) VALUES (:email, :newPassword, :profilePic, :bio) WHERE id = :id');
+                if (!$updateUser) {
+                    die(var_dump($pdo->errorInfo()));
+                }
+
+                $updateUser->bindParam(':email', $email, PDO::PARAM_STR);
+                $updateUser->bindParam(':profilePic', $image, PDO::PARAM_STR);
+                $updateUser->bindParam(':bio', $biography, PDO::PARAM_STR);
+                $updateUser->bindParam(':newPassword', $newPassword, PDO::PARAM_STR);
+                $updateUser->bindParam(':id', $id, PDO::PARAM_INT);
+                $updateUser->execute();
+                $updatedUser = $updateUser->fetch(PDO::FETCH_ASSOC);
+            } 
+            // If errors is not empty
+            else {
+                foreach ($errors as $error) {
+                    echo $error . "<br>";
+            }
+}
+}
+        // If errors is not empty
+        else {
+            foreach ($errors as $error) {
+                echo $error . "<br>";
+        }
+    }
+}
+ // If password is incorrect
+ else{
+    $errors [] = "Incorrect password, try again!";
+}
+}
+// If password is not submitted
+else {
+    $errors [] = "You must submit your password to make these changes.";
+}
+// If there are an errors
+if (!empty($errors)) {
+    foreach ($errors as $error) {
+        echo $error . "<br>";
+    }
+}
--- update-user-page.php
+++ update-user-page.php
@@ -9,54 +9,59 @@
 
     <!-- Display error messages here -->
 
-    <form action="app/users/register.php" method="post">
+    <form action="app/users/update.php" method="post">
+
         <!-- Profile picture -->
         <div class="form-group">
             <label for="image">Image</label>
-            <input class="form-control" type="file" name="image" accept=".jpg, .jpeg, .png">
-            <small class="form-text text-muted">Please insert your image here. Please use following file formats: jpg,
+            <input class="form-control" type="file" name="image" accept=".jpg, .jpeg, .png" value="<?= $loggedInUser['profile_image_url'];?>">
+            <small class="form-text text-muted">Please insert your profile picture here. Please use following file
+                formats: jpg,
                 jpeg, png. Max filesize 2MB.</small>
         </div>
         <!-- Biography -->
         <div class="form-group">
             <label for="email">Biography</label>
-            <input class="form-control" type="bio" name="bio">
+            <input class="form-control" type="bio" name="bio" value="<?= $loggedInUser['bio'];?>">
             <small class="form-text text-muted">Write something about yourself.</small>
         </div>
         <!-- Email -->
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="email@email.com" required>
+            <input class="form-control" type="email" name="email" placeholder="email@email.com" value="<?= $loggedInUser['email'];?>"
+                required>
             <small class="form-text text-muted">Please provide your email address.</small>
         </div>
         <!-- Password -->
         <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide your password.</small>
+            <label for="password">Current Password</label>
+            <input class="form-control" type="password" name="current-password">
+            <small class="form-text text-muted">Please provide your old password.</small>
         </div>
-        <!-- Password-control -->
+        <!-- New password -->
         <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password-control" required>
-            <small class="form-text text-muted">Please provide your password again.</small>
+            <label for="password">New Password</label>
+            <input class="form-control" type="password" name="new-password">
+            <small class="form-text text-muted">Please provide your new password.</small>
         </div>
-        <!-- First name -->
+        <!-- Confirm password -->
         <div class="form-group">
-            <label for="first-name">First name</label>
-            <input class="form-control" type="first-name" name="first-name" required>
+            <label for="password">New Password</label>
+            <input class="form-control" type="password" name="password-control">
+            <small class="form-text text-muted">Please provide your new password again.</small>
+        </div>
+        <!-- Name -->
+        <div class="form-group">
+            <label for="first-name">Name</label>
+            <input class="form-control" type="first-name" name="first-name" value="<?= $loggedInUser['name'];?>"
+                disabled>
             <small class="form-text text-muted">Please provide your first name.</small>
-        </div>
-        <!-- Last name -->
-        <div class="form-group">
-            <label for="last-name">Last name</label>
-            <input class="form-control" type="last-name" name="last-name" required>
-            <small class="form-text text-muted">Please provide your last name.</small>
         </div>
         <!-- Username -->
         <div class="form-group">
             <label for="username">Username</label>
-            <input class="form-control" type="username" name="username" required>
+            <input class="form-control" type="username" name="username" value="<?= $loggedInUser['username'];?>"
+                disabled>
             <small class="form-text text-muted">Please provide your username of choice.</small>
         </div>
         <!-- Submit-button -->

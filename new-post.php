ac172ad12aa26b7ebf728e635a638f2fad865ef3
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-08 14:48:07 +0100

    file clean up

--- new-post.php
+++ new-post.php
@@ -1,27 +1,27 @@
 <?php require __DIR__.'/views/header.php'; 
 
 // This is where the user can adde new posts.
-// Add a functions file to this.
 ?>
 
 <article>
     <h1>Create new post</h1>
 
-<!-- Display error messages here -->
+    <!-- Display error messages here -->
 
     <form action="app/posts/store.php" method="post" enctype="multipart/form-data">
-<!-- Image -->
+        <!-- Image -->
         <div class="form-group">
             <label for="image">Image</label>
             <input class="form-control" type="file" name="image" accept=".jpg, .jpeg, .png" required>
-            <small class="form-text text-muted">Please insert your image here. Please use following file formats: jpg, jpeg, png. Max filesize 2MB.</small>
+            <small class="form-text text-muted">Please insert your image here. Please use following file formats: jpg,
+                jpeg, png. Max filesize 2MB.</small>
         </div>
-<!-- Description -->
+        <!-- Description -->
         <div class="form-group">
             <label for="description">Description</label>
             <input class="form-control" type="text" name="description" placeholder="Write your description here">
         </div>
-<!-- Post-button -->
+        <!-- Post-button -->
         <button type="submit" class="btn btn-primary">Post</button>
     </form>
 </article>
--- views/navigation.php
+++ views/navigation.php
@@ -1,34 +1,41 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+    <a class="navbar-brand" href="#">
+        <?php echo $config['title']; ?></a>
 
-  <ul class="navbar-nav">
-  <!-- Home -->
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/index.php"){echo 'active';};?>" href="./index.php">Home</a>
-      </li>
-<!-- About -->
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/about.php"){echo 'active';};?>" href="./about.php">About- nonexisting</a>
-      </li>
-<!-- My pages -->
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/my-pages.php"){echo 'active';};?>" href="<?php if(isset($_SESSION['user'])){echo "./my-pages.php";};?>">
-          <?php if(isset($_SESSION['user'])){echo "My pages";};?></a>
-      </li>
-<!-- Create post -->
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/new-post.php"){echo 'active';};?>" href="<?php if(isset($_SESSION['user'])){echo "./new-post.php";};?>">
-          <?php if(isset($_SESSION['user'])){echo "Create post";};?></a>
-      </li>
-<!-- Login/Logout -->
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/login-page.php"){echo 'active';};?>" href="<?php if(isset($_SESSION['user'])){echo "./app/users/logout.php";} else{echo "./login-page.php";};?>">
-          <?php if(isset($_SESSION['user'])){echo "Logout";} else{echo "Login";};?></a>
-      </li>
-<!-- Register -->
-      <li class="nav-item">
-        <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/register-page.php"){echo 'active';};?>" href="<?php if(!isset($_SESSION['user'])){echo "./register-page.php";};?>">
-        <?php if(!isset($_SESSION['user'])){echo "Register";};?></a>
-      </li>
-  </ul>
+    <ul class="navbar-nav">
+        <!-- Home -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/index.php"){echo 'active' ;};?>"
+                href="./index.php">Home</a>
+        </li>
+        <!-- About -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/about.php"){echo 'active' ;};?>"
+                href="./about.php">About- nonexisting</a>
+        </li>
+        <!-- My pages -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/my-pages.php"){echo 'active' ;};?>" href="
+                <?php if(isset($loggedInUser)){echo "./my-pages.php";};?>">
+                <?php if(isset($loggedInUser)){echo "My pages";};?></a>
+        </li>
+        <!-- Create post -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/new-post.php"){echo 'active' ;};?>" href="
+                <?php if(isset($loggedInUser)){echo "./new-post.php";};?>">
+                <?php if(isset($loggedInUser)){echo "Create post";};?></a>
+        </li>
+        <!-- Login/Logout -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/login-page.php"){echo 'active' ;};?>" href="
+                <?php if(isset($loggedInUser)){echo "./app/users/logout.php";} else{echo "./login-page.php";};?>">
+                <?php if(isset($loggedInUser)){echo "Logout";} else{echo "Login";};?></a>
+        </li>
+        <!-- Register -->
+        <li class="nav-item">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/register-page.php"){echo 'active' ;};?>" href="
+                <?php if(!isset($loggedInUser)){echo "./register-page.php";};?>">
+                <?php if(!isset($loggedInUser)){echo "Register";};?></a>
+        </li>
+    </ul>
 </nav>

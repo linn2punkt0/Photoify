a1cf6ed2d3665774c87610babb8d7b94c2077bf3
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-08 14:49:52 +0100

    added post feed to index

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<h2><?php if(isset($_SESSION['user'])){echo "You are logged in!\n";} ?></h2>
-    <p>This is the home page.</p>
-
+<h2>
+    <?php if(isset($_SESSION['user'])){echo "You are logged in!\n";} ?>
+</h2>
+<p>This is the home page.</p>
+<?php require __DIR__.'/feed.php'; ?>
 <?php require __DIR__.'/views/footer.php'; ?>

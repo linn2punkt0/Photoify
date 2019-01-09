49cd578a5169d3272ae4d962bb0bd112edc2116b
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-09 15:01:07 +0100

    adjusted update-file

--- app/users/update-V2.php
+++ app/users/update-V2.php
@@ -4,97 +4,88 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// In this file we register users.
+// In this file we update users.
 
 $errors = [];
+$biography = null;
+$image = null;
 
-// Check if current password is submitted
-if ($_POST['current-password']) {
+
+//                  PASSWORD VERIFICATION
+
+// Check if current password is submitted, if not add error message, otherwise, continue
+if (!isset($_POST['current-password'])) {
+    $errors[] = "You must submit your password to make these changes.";
+}
     
-// Check if submitted password matches database
-    if(password_verify($_POST['current-password'], $loggedInUser['password'])){
+// Check if submitted password matches database, if not add error message, otherwise, continue
+if(!password_verify($_POST['current-password'], $loggedInUser['password'])){
+    $errors[] = "Incorrect password, try again!";
+}
+
+//                  IMAGE-SECTION
 
 // Check if image is submitted
-    if (isset($_FILES['image'])) {
-        $image = $_FILES['image'];
+if (isset($_FILES['image'])) {
+    $image = uploadImage($_FILES['image']);
+}
 
-    // Check if image is correct file type, if not, store error message
-        if (!in_array($image['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
-            $errors[] = 'The uploaded file type is not allowed.';
+// Check if the errors any contains any errors concerning the image, if not, continue
+if (count($errors) === 0) {
+
+    if (isset($_POST['bio'])) {
+        $biography = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
+    }
+    
+    // Sanitize all fields
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $id = $loggedInUser['id'];
+    
+    // Fetch user my email
+    $user = getUserByEmail($email);
+
+    // If submitted email already exists in database, look if it's logged in user, or other user.
+    if ($user) {
+    
+        // If email belongs to other user, add message to errors array
+        if ($user['id'] !== $id) {
+         $errors[] = "Email is used by other account!";
+        }
+        
+    // If email belongs to logged in user, proceed and use email in update-statement
+    }
+    
+    if (isset($_POST['new-password'])) {
+
+        // If passwords do not match, add message to errors array
+        if ($_POST['new-password'] !== $_POST['password-control']) {
+            $errors[] = "New passwords did not match, try again!";
         }
 
-    // Check if image is correct size, if not, store error message
-        if ($image['size'] > 2097152) {
-            $errors[] = 'The uploaded file exceeded the filesize limit.';
+        // If new passwords match, save the new password
+        else {
+            $password = password_hash($_POST['new-password'], PASSWORD_BCRYPT);
         }
     }
     
-    // Check if the errors any contains any errors concerning the image, if not, continue
-        if (count($errors) === 0) {
+    // Check if the errors any contains any errors, if not, continue
+    if (count($errors) === 0) {
 
-        // Sanitize all fields
-            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-            $biography = filter_var($_POST['bio'], FILTER_SANITIZE_STRING);
-            $password = password_hash($_POST['new-password'], PASSWORD_BCRYPT);
-            $id = $loggedInUser['id'];
+    updateUser($email, $image, $biography, $password, $id);
+    $updatedUser = getUserByEmail($email);
+    
+    // Redirect user to "My pages" to see their newly uploaded image           
+    redirect('../../my-pages.php');
+    }
 
-            $user = getUserByEmail($email);
-            die(var_dump($user));
-            if ($user) {
-                # code...
-            }
-
-        // If email exists in other account, or if passwords do not match, add message to errors array.
-            if ($notNewEmail && !$id) {
-                $errors [] = "Email is used by other account!";
-            }
-            if ($_POST['new-password'] !== $_POST['password-control']) {
-                $errors [] = "New passwords did not match, try again!";
-            }
-        // If new passwords match, save the new password
-            else {
-                $newPassword = $_POST['new-password'];
-            }
-            
-        // Check if the errors any contains any errors, if not, continue
-            if (count($errors) === 0) {
-
-                $updateUser = $pdo->prepare('UPDATE users (email, "password", profile_image_url, biography) VALUES (:email, :newPassword, :profilePic, :bio) WHERE id = :id');
-                if (!$updateUser) {
-                    die(var_dump($pdo->errorInfo()));
-                }
-
-                $updateUser->bindParam(':email', $email, PDO::PARAM_STR);
-                $updateUser->bindParam(':profilePic', $image, PDO::PARAM_STR);
-                $updateUser->bindParam(':bio', $biography, PDO::PARAM_STR);
-                $updateUser->bindParam(':newPassword', $newPassword, PDO::PARAM_STR);
-                $updateUser->bindParam(':id', $id, PDO::PARAM_INT);
-                $updateUser->execute();
-                $updatedUser = $updateUser->fetch(PDO::FETCH_ASSOC);
-            } 
-            // If errors is not empty (email or password-errors)
-            else {
-                foreach ($errors as $error) {
-                    echo $error . "<br>";
-            }
-}
-}
-        // If errors is not empty (image-errors)
-        else {
-            foreach ($errors as $error) {
-                echo $error . "<br>";
+    // If errors is not empty (email or password-errors)
+    else {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
         }
     }
-}
- // If password is incorrect
- else{
-    $errors [] = "Incorrect password, try again!";
-}
-}
-// If password is not submitted
-else {
-    $errors [] = "You must submit your password to make these changes.";
-}
+} 
+    
 // If there are an errors
 if (!empty($errors)) {
     foreach ($errors as $error) {

74877efab3c493613c7146bed3b76730ebaab029
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-10 14:16:20 +0100

    commented out unfinished js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
-const likeButton = document.querySelector(".like-button");
-const dislikeButton = document.querySelector(".dislike-button");
+// const likeButton = document.querySelector(".like-button");
+// const dislikeButton = document.querySelector(".dislike-button");
 
-likeButton.addEventListener("click", addLikes);
-dislikeButton.addEventListener("click", addDislikes);
+// likeButton.addEventListener("click", addLikes);
+// dislikeButton.addEventListener("click", addDislikes);

adb5f8ac92a7d200053c9f644151c29bdb28e0e4
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-01-16 17:38:46 +0100

    adding new menu for nice mobile version

--- views/navigation.php
+++ views/navigation.php
@@ -2,6 +2,7 @@
     <a class="navbar-brand" href="#">
         <?php echo $config['title']; ?></a>
 
+    <!-- THIS IS ONLY FOR DESKTOP -->
     <ul class="navbar-nav">
         <!-- Home -->
         <li class="nav-item">
@@ -9,16 +10,14 @@
                 href="./index.php">Home</a>
         </li>
         <!-- My pages -->
-        <li class="nav-item">
-            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/my-pages.php"){echo 'active' ;};?>" href="
-                <?php if(isset($loggedInUser)){echo "./my-pages.php";};?>">
-                <?php if(isset($loggedInUser)){echo "My pages";};?></a>
+        <li class="nav-item <?php if(!isset($loggedInUser)){echo 'hide';};?>">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/my-pages.php"){echo 'active' ;};?>"
+                href="./my-pages.php">My pages</a>
         </li>
         <!-- Create post -->
-        <li class="nav-item">
-            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/new-post.php"){echo 'active' ;};?>" href="
-                <?php if(isset($loggedInUser)){echo "./new-post.php";};?>">
-                <?php if(isset($loggedInUser)){echo "Create post";};?></a>
+        <li class="nav-item <?php if(!isset($loggedInUser)){echo 'hide';};?>">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/new-post.php"){echo 'active' ;};?>"
+                href="./new-post.php">Create post</a>
         </li>
         <!-- Login/Logout -->
         <li class="nav-item">
@@ -27,10 +26,46 @@
                 <?php if(isset($loggedInUser)){echo "Logout";} else{echo "Login";};?></a>
         </li>
         <!-- Register -->
-        <li class="nav-item">
-            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/register-page.php"){echo 'active' ;};?>" href="
-                <?php if(!isset($loggedInUser)){echo "./register-page.php";};?>">
-                <?php if(!isset($loggedInUser)){echo "Register";};?></a>
+        <li class="nav-item <?php if(isset($loggedInUser)){echo 'hide';};?>">
+            <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/register-page.php"){echo 'active' ;};?>"
+                href="./register-page.php">Register</a>
         </li>
     </ul>
+
+    <!-- THIS IS ONLY FOR MOBILE -->
+    <div class="nav-icon1">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div class="mobile-menu">
+        <ul class="mobile-nav">
+            <!-- Home -->
+            <li class="nav-item">
+                <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/index.php"){echo 'active' ;};?>"
+                    href="./index.php">Home</a>
+            </li>
+            <!-- My pages -->
+            <li class="nav-item <?php if(!isset($loggedInUser)){echo 'hide';};?>">
+                <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/my-pages.php"){echo 'active' ;};?>"
+                    href="./my-pages.php">My pages</a>
+            </li>
+            <!-- Create post -->
+            <li class="nav-item <?php if(!isset($loggedInUser)){echo 'hide';};?>">
+                <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/new-post.php"){echo 'active' ;};?>"
+                    href="./new-post.php">Create post</a>
+            </li>
+            <!-- Login/Logout -->
+            <li class="nav-item">
+                <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/login-page.php"){echo 'active' ;};?>" href="
+                    <?php if(isset($loggedInUser)){echo "./app/users/logout.php";} else{echo "./login-page.php";};?>">
+                    <?php if(isset($loggedInUser)){echo "Logout";} else{echo "Login";};?></a>
+            </li>
+            <!-- Register -->
+            <li class="nav-item <?php if(isset($loggedInUser)){echo 'hide';};?>">
+                <a class="nav-link <?php if($_SERVER['PHP_SELF'] === "/register-page.php"){echo 'active' ;};?>"
+                    href="./register-page.php">Register</a>
+            </li>
+        </ul>
+    </div>
 </nav>
